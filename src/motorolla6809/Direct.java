74645cf22e10c66bc0125c9915ecfb5243499109
ayoubGO <ayoub.gouhassoun@gmail.com>
2025-10-25 19:40:53 +0100

    version

--- src/motorolla6809/Direct.java
+++ src/motorolla6809/Direct.java
@@ -32,7 +32,7 @@ public class Direct {
 		{JOptionPane.showMessageDialog(
                 null,
                 "Erreur : Votre programme comporte des erreurs",
-                "Valeur invalidddddddddddddddddddde",
+                "Valeur invaliddhhhhdddddddddddddddddde",
                 JOptionPane.ERROR_MESSAGE
             );}
 		else
